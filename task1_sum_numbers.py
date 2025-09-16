edff39bd569c83cafd5da1fb1fb83661ec2f32d7
Abrar Patel <111578824+Abrarpat@users.noreply.github.com>
2025-09-16 22:37:09 +0530

    Add files via upload

---
+++ task1_sum_numbers.py
@@ -0,0 +1,13 @@
+# Taking input from the user
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+# Displaying input
+print("\nYou entered:", first_number, "and", second_number)
+
+# Operations
+print("The sum is:", first_number + second_number)
+print("The difference is:", first_number - second_number)
+print("The product is:", first_number * second_number)
+
+
---
+++ task2_greeting.py
@@ -0,0 +1,3 @@
+a=input('enter your first name: ')
+b=input('enter your last name: ')
+print("hello " +  a  +  b +"! Welcom to the python program.")
